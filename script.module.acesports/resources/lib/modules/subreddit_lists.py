4e492e537095f0653691f3bb0b00c5727b20d3de
Johnston <iainjohnston1979@gmail.com>
2018-05-12 14:35:06 +0100

    update

---
+++ script.module.acesports/resources/lib/modules/subreddit_lists.py
@@ -0,0 +1,7 @@
+
+streaming_subreddits = [
+{'name': 'Boxing Streams', 'url': 'BoxingStreams'},
+{'name': 'Rugby Streams', 'url': 'RugbyStreams'},
+{'name': 'Motor Sports Streams', 'url': 'motorsportsstreams'},
+{'name': 'Live Tv Links', 'url': 'LiveTvLinks'},
+]
