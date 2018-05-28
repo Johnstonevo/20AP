7bd7fdaec2535b6e518acd17a42e6e95da0bb072
Johnston <iainjohnston1979@gmail.com>
2018-05-28 21:42:08 +0100

    update

--- plugin.video.20ap/default.py
+++ plugin.video.20ap/default.py
@@ -25,11 +25,11 @@ addon_id = xbmcaddon.Addon().getAddonInfo('id')
 ownAddon = xbmcaddon.Addon(id=addon_id)
 enable_installa = ownAddon.getSetting('dlimage')
 enable_newswin = ownAddon.getSetting('news_win')
-root_xml_url = ownAddon.getSetting('root_xml')
-__builtin__.tvdb_api_key = ownAddon.getSetting('tvdb_api_key')
-__builtin__.tmdb_api_key = ownAddon.getSetting('tmdb_api_key')
-__builtin__.trakt_client_id = ownAddon.getSetting('trakt_api_client_id')
-__builtin__.trakt_client_secret = ownAddon.getSetting('trakt_api_client_secret')
+root_xml_url = "https://raw.githubusercontent.com/Johnstonevo/20ap/master/base/20AP/base/base.xml"  # url of the root xml file
+__builtin__.tvdb_api_key = "D59DC0B30C99333E"  # tvdb api key
+__builtin__.tmdb_api_key = "aa28550e5a65f567fc512bd0290ce6fb"  # tmdb api key
+__builtin__.trakt_client_id = "826f424de711d416abc0f2d9ffae98406dd2489ab7a7f1089b9635ae3c5476ed"  # trakt client id
+__builtin__.trakt_client_secret = "3e3c7a3f4f5c2f09154a4759e4cdd23c33b055e0c492d2c635f05539c974a0f6"  # trakt client secret
 __builtin__.search_db_location = ownAddon.getSetting('search_db_location')
 
 import os
