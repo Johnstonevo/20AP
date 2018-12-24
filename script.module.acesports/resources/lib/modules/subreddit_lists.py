aaa1808b3c5131093d3423398af3b60520e71b8a
Johnstonevo <ig_johnston@hotmail.com>
2018-12-24 12:52:48 +0000

    submodules

---
+++ script.module.acesports/resources/lib/modules/subreddit_lists.py
@@ -0,0 +1,8 @@
+streaming_subreddits = [
+{'name': 'Boxing Streams', 'url': 'BoxingStreams'},
+{'name': 'Rugby Streams', 'url': 'RugbyStreams'},
+{'name': 'Motor Sports Streams', 'url': 'motorsportsstreams'},
+{'name': 'Soccer Streams', 'url': 'soccerstreams'},
+{'name': 'MMA Streams', 'url': 'MMAStreams'},
+]
+
