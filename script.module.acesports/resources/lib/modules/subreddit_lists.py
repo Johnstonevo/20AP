62d14a9984276d4670cf6b2f5912a737a8fd1be7
Iain Johnston <you@example.com>
2019-02-09 14:08:49 +0000

    update

--- script.module.acesports/resources/lib/modules/subreddit_lists.py
+++
@@ -1,8 +0,0 @@
-streaming_subreddits = [
-{'name': 'Boxing Streams', 'url': 'BoxingStreams'},
-{'name': 'Rugby Streams', 'url': 'RugbyStreams'},
-{'name': 'Motor Sports Streams', 'url': 'motorsportsstreams'},
-{'name': 'Soccer Streams', 'url': 'soccerstreams'},
-{'name': 'MMA Streams', 'url': 'MMAStreams'},
-]
-
